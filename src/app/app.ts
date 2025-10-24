b999c80c2c70c4b2bea5a6c6608602909c0db536
Paulo Souza <pauloo.bsb@gmail.com>
2025-10-24 18:33:10 -0300

    Primeiro commit do portfólio

---
+++ src/app/app.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import * as core from '@angular/core';
+
+@core.Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'] // ✅ Mantido no plural
+})
+export class App implements core.AfterViewInit {
+
+  // Lifecycle hook — executa após o template ser carregado
+  ngAfterViewInit(): void {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+
+    if (menuToggle && navMenu) {
+      menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        menuToggle.classList.toggle('open');
+      });
+    } else {
+      console.warn('Elementos do menu não encontrados no DOM.');
+    }
+  }
+}
